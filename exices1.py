15c463b759c0b6f14dc5fb8f6728336c2320f508
bholekurmi <93009701+bholekurmi@users.noreply.github.com>
2025-07-25 17:33:51 +0530

    Add files via upload

---
+++ exices1.py
@@ -0,0 +1,16 @@
+#print("rk\n12 \nNnd").
+
+#a = 10.5
+#b = int(a)
+#print (b)
+#print (type(b))
+
+#i = int(input("id\n"))
+#print(i)
+
+#i = int(input("enter the interger value"))
+#i =float(i)
+#print (i)
+#print(type(i))
+
+
---
+++ operators.py
@@ -0,0 +1,6 @@
+#a= 29%13
+#a =61//6
+#print(a)
+
+print(3>4 and 7>4)
+print(3)
